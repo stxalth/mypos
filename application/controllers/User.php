cb802f8258d1567919f08611502cf596a76069fd
stxalth <azz.malez@gmail.com>
2021-12-21 00:41:23 +0700

    Add User Data And Form Validation

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,18 +2,52 @@
 
 class User extends CI_Controller
 {
+    function __construct()
+    {
+        parent::__construct();
+        check_not_login();
+        $this->load->model('user_m');
+    }
 
     public function index()
     {
-        check_not_login();
-
-        $this->load->model('user_m');
         $data['row'] = $this->user_m->get();
         $this->template->load('template', 'user/user_data', $data);
     }
 
     public function add()
     {
-        $this->template->load('template', 'user/user_form_add');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('fullname', 'Nama', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[user.username]');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[5]');
+        $this->form_validation->set_rules(
+            'passconf',
+            'Konfirmasi Password',
+            'required|matches[password]',
+            array('matches' => '%s tidak sesuai dengan password')
+        );
+        $this->form_validation->set_rules('level', 'Level', 'required');
+
+        $this->form_validation->set_message('required', '%s masih kosong, mohon diisi terlebih dahulu');
+        $this->form_validation->set_message('min_length', '%s minimal 5 karakter');
+        $this->form_validation->set_message('is_unique', '%s ini sudah dipakai, silahkan ganti');
+        // %s digunakan untuk menunjuk sebuah alias dalam parameter. Misalnya 'Level' pada level
+        // %s bisa diganti dengan {field}
+
+        $this->form_validation->set_error_delimiters('<span class="help-block">', "</span>");
+        // set_error_delimiters untuk membuat custom error messages
+        // sehingga tidak perlu membuat custom error pada masing-masing div di dalam view
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->template->load('template', 'user/user_form_add');
+        } else {
+            $post = $this->input->post(null, TRUE);
+            $this->user_m->add($post);
+            if ($this->db->affected_rows() > 0) {
+                echo "<script>alert('Data berhasil disimpan');</script>";
+            }
+            echo "<script>window.location='" . site_url('user') . "'</script>";
+        }
     }
 }
