b5eeee95a7d2086e6d09d76437be6422026346cd
stxalth <azz.malez@gmail.com>
2021-12-20 15:00:48 +0700

    User Login

---
+++ application/models/User_m.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User_m extends CI_Model
+{
+
+    public function login($post)
+    {
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->where('username', $post['username']);
+        $this->db->where('password', sha1($post['password'])); //tambahin "sha1" sesuai di databasenya
+        $query = $this->db->get();
+        return $query;
+    }
+}
