6148c88e66b0dca66052825f4fa6bfb382e102a0
stxalth <azz.malez@gmail.com>
2021-12-22 02:22:56 +0700

    Tampil dan Hapus Data Supplier

--- application/views/template.php
+++ application/views/template.php
@@ -125,7 +125,7 @@
                         <a href="<?= site_url('dashboard') ?>"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
                     </li>
                     <li>
-                        <a href=""><i class="fa fa-truck"></i> <span>Supplier</span></a>
+                        <a href="<?= site_url('supplier') ?>"><i class="fa fa-truck"></i> <span>Supplier</span></a>
                     </li>
                     <li>
                         <a href=""><i class="fa fa-users"></i> <span>Customers</span></a>
